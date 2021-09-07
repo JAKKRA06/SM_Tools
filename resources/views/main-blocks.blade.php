599e9b33326a93715072e0af26b4404c9958e06f
Kubson <jakub.krajniewski@salesmanago.com>
2021-09-07 21:08:08 +0200

    Home view in progress

--- resources/views/main-blocks.blade.php
+++ resources/views/main-blocks.blade.php
@@ -1,14 +1,24 @@
 @extends('start')
 
 @section('content')
-<div class="row main-blocks">
-    <div class="col-6 col-sm-3">
-        <a class="nav-link" href="{{ URL::to('faq') }}">FAQ</a>
-    </div>
-    <div class="col-6 col-sm-3">
-        STATYSTYKI
-    </div>
-</div>
-    <div>
-    </div>
+    <section class="main-slider">
+        <div class="d-flex p-2">
+        </div>
+    </section>
+
+        <div class="row">
+            <div class="col-6 faq-box main-boxes">
+                <a class="nav-link" href="{{ URL::to('faq') }}">FAQ</a>
+            </div>
+            <div class="col-6 statistics-box main-boxes">
+                <a class="nav-link">STATISTICS</a>
+            </div>
+
+            <div class="col-6 resources-box main-boxes">
+                <a class="nav-link">RESOURCES</a>
+            </div>
+            <div class="col-6 tools-box main-boxes">
+                <a class="nav-link">TOOLS</a>
+            </div>
+        </div>
 @endsection('content')
--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -6,48 +6,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ URL::asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link href="{{ URL::to('assets/css/style.css') }}" rel="stylesheet">
+{{--    <link rel="stylesheet" href="{{ URL::asset('assets/css/style.css') }}">--}}
+{{--    <link rel="stylesheet" type="text/css" href="{{ URL::to('css/style.css') }}">--}}
     <title>SM Tools!</title>
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-inverse">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/">SALESmanago tools</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ URL::to('faq') }}">FAQ</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ URL::to('faq') }}">Inne</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Rozwiń mnie!
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-            </ul>
+        <div class="navbar-header">
+            <a class="navbar-brand" href="{{ URL::to('/') }}">SALESmanago Tools</a>
         </div>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="{{ URL::to('faq') }}"><span class="glyphicon glyphicon-question-sign"></span> FAQ</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-stats"></span> STATISTICS</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-folder-open"></span> RESOURCES</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-wrench"></span> TOOLS</a></li>
+        </ul>
     </div>
 </nav>
 
-
-<div class="container align-items-center">
+<main class="container align-items-center">
     @yield('content')
-</div>
-
-
-
+</main>
 
 
 <!-- Option 1: Bootstrap Bundle with Popper -->
